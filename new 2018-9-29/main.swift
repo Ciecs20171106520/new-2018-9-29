1ca55f959a74342b4d1fed261df3e9ac81ba1de8
Ciecs20171106520 <694522707@qq.com>
2018-09-29 08:29:32 +0800

    Initial Commit

---
+++ new 2018-9-29/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  new 2018-9-29
+//
+//  Created by s20171106520 on 2018/9/29.
+//  Copyright © 2018年 s20171106520. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
